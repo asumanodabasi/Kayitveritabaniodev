ddd8757a2e06cb2dc75c466a013cf911bf2758f4
Asuman Odabaşı <asuman.odabasi@ogr.sakarya.edu.tr>
2024-12-22 21:57:14 +0300

    Proje dosyası ekle.

---
+++ veritabaniodev/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace veritabaniodev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtrandevu = new System.Windows.Forms.TextBox();
+            this.txtrandevutarih = new System.Windows.Forms.TextBox();
+            this.txtodeme = new System.Windows.Forms.TextBox();
+            this.txtodemesekli = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(761, 435);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(855, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Randevu ID:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(855, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Doktor  İsmi:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(846, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Memnuniyet";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(788, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Randevu Durumu:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(865, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Polikinlik:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(815, 294);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Randevu Tarihi:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(825, 338);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Odenen Tutar:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(827, 386);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Odeme Şekli:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(855, 147);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Hasta İsmi:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtrandevu
+            // 
+            this.txtrandevu.Location = new System.Drawing.Point(1010, 9);
+            this.txtrandevu.Name = "txtrandevu";
+            this.txtrandevu.Size = new System.Drawing.Size(136, 30);
+            this.txtrandevu.TabIndex = 10;
+            // 
+            // txtrandevutarih
+            // 
+            this.txtrandevutarih.Location = new System.Drawing.Point(1010, 289);
+            this.txtrandevutarih.Name = "txtrandevutarih";
+            this.txtrandevutarih.Size = new System.Drawing.Size(136, 30);
+            this.txtrandevutarih.TabIndex = 11;
+            // 
+            // txtodeme
+            // 
+            this.txtodeme.Location = new System.Drawing.Point(1010, 338);
+            this.txtodeme.Name = "txtodeme";
+            this.txtodeme.Size = new System.Drawing.Size(136, 30);
+            this.txtodeme.TabIndex = 12;
+            // 
+            // txtodemesekli
+            // 
+            this.txtodemesekli.Location = new System.Drawing.Point(1010, 381);
+            this.txtodemesekli.Name = "txtodemesekli";
+            this.txtodemesekli.Size = new System.Drawing.Size(136, 30);
+            this.txtodemesekli.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1010, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 33);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(1010, 104);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(136, 33);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(1010, 147);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(136, 33);
+            this.comboBox3.TabIndex = 16;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(1010, 190);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(136, 33);
+            this.comboBox4.TabIndex = 17;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(1010, 237);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(136, 33);
+            this.comboBox5.TabIndex = 18;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(889, 446);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(257, 35);
+            this.btnListele.TabIndex = 19;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(889, 501);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(257, 35);
+            this.btnEkle.TabIndex = 20;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(889, 615);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(257, 35);
+            this.btnGuncelle.TabIndex = 21;
+            this.btnGuncelle.Text = "Guncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(889, 557);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(257, 35);
+            this.btnSil.TabIndex = 22;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtodemesekli);
+            this.Controls.Add(this.txtodeme);
+            this.Controls.Add(this.txtrandevutarih);
+            this.Controls.Add(this.txtrandevu);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtrandevu;
+        private System.Windows.Forms.TextBox txtrandevutarih;
+        private System.Windows.Forms.TextBox txtodeme;
+        private System.Windows.Forms.TextBox txtodemesekli;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ veritabaniodev/Form1.cs
@@ -0,0 +1,144 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veritabaniodev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        NpgsqlConnection baglantı = new NpgsqlConnection("server=localHost; port=5432; Database=vytsodev; user Id=postgres; password=12345");
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            string listeleme = "select* from randevu";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(listeleme, baglantı);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            NpgsqlCommand ekleme = new NpgsqlCommand("insert into randevu(randevuid,doktorid,memnuniyetdurumid,hastaid,durumid,polikinlikid,randevutarihi,odenentutar,odemesekli)  values(@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8,@p9)", baglantı);
+
+            ekleme.Parameters.AddWithValue("@p1", int.Parse(txtrandevu.Text));
+            ekleme.Parameters.AddWithValue("@p2", int.Parse(comboBox1.SelectedValue.ToString()));
+            ekleme.Parameters.AddWithValue("@p3", int.Parse(comboBox2.SelectedValue.ToString()));
+            ekleme.Parameters.AddWithValue("@p4", int.Parse(comboBox3.SelectedValue.ToString()));
+            ekleme.Parameters.AddWithValue("@p5", int.Parse(comboBox4.SelectedValue.ToString()));
+            ekleme.Parameters.AddWithValue("@p6", int.Parse(comboBox5.SelectedValue.ToString()));
+
+            ekleme.Parameters.AddWithValue("@p7", txtrandevutarih.Text);
+            ekleme.Parameters.AddWithValue("@p8", int.Parse(txtodeme.Text));
+            ekleme.Parameters.AddWithValue("@p9", txtodemesekli.Text);
+
+
+            ekleme.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("urun ekleme işlemi başarılı bir şekilde gerçekleştir");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            NpgsqlDataAdapter d1 = new NpgsqlDataAdapter("select * from doktor", baglantı);
+            DataTable dt = new DataTable();
+            d1.Fill(dt);
+            comboBox1.DisplayMember = "personelad";
+            comboBox1.ValueMember = "personelid";
+            comboBox1.DataSource = dt;
+            baglantı.Close();
+
+
+            baglantı.Open();
+            NpgsqlDataAdapter d2 = new NpgsqlDataAdapter("select * from memnuniyetdurum", baglantı);
+            DataTable ds = new DataTable();
+            d2.Fill(ds);
+            comboBox2.DisplayMember = "memnuniyetAd";
+            comboBox2.ValueMember = "memnuniyetid";
+            comboBox2.DataSource = ds;
+            baglantı.Close();
+
+
+            baglantı.Open();
+            NpgsqlDataAdapter d3 = new NpgsqlDataAdapter("select * from hasta", baglantı);
+            DataTable dm = new DataTable();
+            d3.Fill(dm);
+            comboBox3.DisplayMember = "hastaAd";
+            comboBox3.ValueMember = "hastaid";
+            comboBox3.DataSource = dm;
+            baglantı.Close();
+
+
+            baglantı.Open();
+            NpgsqlDataAdapter d4 = new NpgsqlDataAdapter("select * from randevudurum", baglantı);
+            DataTable dd = new DataTable();
+            d4.Fill(dd);
+            comboBox4.DisplayMember = "durumAd";
+            comboBox4.ValueMember = "ranedevuDurumid";
+            comboBox4.DataSource = dd;
+            baglantı.Close();
+
+
+            baglantı.Open();
+            NpgsqlDataAdapter d5 = new NpgsqlDataAdapter("select * from polikinlik", baglantı);
+            DataTable da = new DataTable();
+            d5.Fill(da);
+            comboBox5.DisplayMember = "polikinlikAd";
+            comboBox5.ValueMember = "polikinlikid";
+            comboBox5.DataSource = da;
+            baglantı.Close();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            NpgsqlCommand silme = new NpgsqlCommand("Delete from randevu Where randevuid =@p1", baglantı);
+            silme.Parameters.AddWithValue("@p1", int.Parse(txtrandevu.Text));
+            silme.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("Silme İslemi Basariyla Tamamlandı", "Bilgi", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglantı.Open();
+            NpgsqlCommand guncelle = new NpgsqlCommand("Update randevu set doktorid=@p2,memnuniyetdurumid=@p3,hastaid=@p4,durumid=@p5,polikinlikid=@p6, randevutarihi=@p7, odenentutar=@p8,odemesekli=@p9 where randevuid=@p1", baglantı);
+            guncelle.Parameters.AddWithValue("@p2", int.Parse(comboBox1.SelectedValue.ToString()));
+            guncelle.Parameters.AddWithValue("@p1", int.Parse(txtrandevu.Text));
+            guncelle.Parameters.AddWithValue("@p3", int.Parse(comboBox2.SelectedValue.ToString()));
+            guncelle.Parameters.AddWithValue("@p4", int.Parse(comboBox3.SelectedValue.ToString()));
+            guncelle.Parameters.AddWithValue("@p5", int.Parse(comboBox4.SelectedValue.ToString()));
+            guncelle.Parameters.AddWithValue("@p6", int.Parse(comboBox5.SelectedValue.ToString()));
+
+            guncelle.Parameters.AddWithValue("@p7", txtrandevutarih.Text);
+            guncelle.Parameters.AddWithValue("@p8", int.Parse(txtodeme.Text));
+            guncelle.Parameters.AddWithValue("@p9", txtodemesekli.Text);
+
+            guncelle.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("Guncelleme islemi tamamlandı", "Bilgi", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+        }
+    }
+}
